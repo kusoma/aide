4fc80db7041b44432b22b8d40193330ef6ea6ba5
Blake <bspencer16@apu.edu>
2020-02-11 18:45:46 -0800

    Bug fixes for startup

---
+++ aide/screens/EventDetails.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { Constant, GlobalStyle } from "../utils/Variables";
+import { TextField } from "../components/Form";
+import { WideButton } from "../components/Buttons";
+
+
+export default class EventDetails extends Component {
+    render() {
+        return (
+            <View>
+                <View>
+                    <Text>Event Details</Text>
+                </View>
+
+
+
+            </View>
+        );
+    }
+}
