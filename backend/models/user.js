46c8a1f85aaabec395f359da46fdc03940cbde33
Mario Lopez <mariolopez@Marios-MacBook-Pro-2.local>
2020-02-23 23:57:17 -0800

    Edit user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,35 +2,38 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true
-
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    canvasToken: {
-        type: String,
-        required: false
-    },
-    googleToken: {
-        type: String,
-        required: false
-    },
-    studyPreference: {
-        type: Schema.Types.ObjectId,
-        ref: 'StudyPreference'
-    }
-})
+	firstName: {
+		type: String,
+		required: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	date_added: {
+		type: Date,
+		default: Date.now,
+	},
+	canvasToken: {
+		type: String,
+		required: false,
+	},
+	googleToken: {
+		type: String,
+		required: false,
+	},
+	studyPreference: {
+		type: Schema.Types.ObjectId,
+		ref: 'StudyPreference',
+	},
+});
 
 module.exports = mongoose.model('User', userSchema);
